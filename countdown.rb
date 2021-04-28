9d6c24c237c92c14734a42340dc09386aebab220
Codi Kraus <codi.kraus@gmail.com>
2021-04-28 21:45:33 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,7 +1,7 @@
 #write your code here
 def countdown(number)
   while number >= 0
-    puts "#{count} SECOND(S)!"
+    puts "#{number} SECOND(S)!"
     number -= 1
   end
   return "HAPPY NEW YEAR!"
