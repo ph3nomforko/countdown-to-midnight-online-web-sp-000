afe0911fd3693a4a535de4224eaa6ca21fd2fc83
Codi Kraus <codi.kraus@gmail.com>
2021-04-28 21:47:56 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -11,7 +11,7 @@ def countdown_with_sleep(number)
   while number > 0
     puts "#{number} SECOND(S)!"
     number -= 1
-    sleeps(1.seconds)
+    sleeps(1.secs)
   end
   return "HAPPY NEW YEAR!"
 end
