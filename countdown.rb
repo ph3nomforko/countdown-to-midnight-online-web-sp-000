f7c7d68630298a8126655d3d3610b97b7b7a650e
Codi Kraus <codi.kraus@gmail.com>
2021-04-28 21:42:04 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,9 @@
 #write your code here
-
-def countdown
+def countdown(number)
+  count = number
+  until integer = 0
+    puts "#{number} SECOND(S)!"
+    count -= 1
+  end
+  return "HAPPY NEW YEAR!"
 end
